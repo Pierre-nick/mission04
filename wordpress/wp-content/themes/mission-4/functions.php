a07a43de4d1e5e75f872fd777d0a0aeb032842c9
Pierre-Nick <pierre-nicolas.martinez@orange.fr>
2021-03-25 13:14:58 +0100

    forcer le chargement de la feuille de style

--- wordpress/wp-content/themes/mission-4/functions.php
+++ wordpress/wp-content/themes/mission-4/functions.php
@@ -1,6 +1,12 @@
 <?php
-    // add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-    // function enqueue_parent_styles() {
-    //     wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
-    // }
+    add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+    function enqueue_parent_styles() {
+        wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+    }
+
+    function register_child_theme_styles() {
+        wp_register_style("style",get_stylesheet_uri());
+        wp_enqueue_style("style");
+    }
+    add_action("wp_enqueue_scripts","register_child_theme_styles");
 ?>
